338ab4f956c43c3f1bca36d4a6851230caf070b2
kostalavista <paradoxnumber7@gmail.com>
2020-12-02 13:10:40 +0200

    Added get address info for many addresses

--- src/api.js
+++ src/api.js
@@ -8,19 +8,50 @@ router.get('/address/:address', (req, res) => {
 	const address = req.params.address;
 
 	Addresses.findOne({address}).then(result => {
-		const data = generateAddressData(address);
-
 		return new Promise(resolve => {
 			if (result) {
 				res.send(result);
 				resolve();
 			} else {
+				const data = generateAddressData(address);
 				insertIntoDatabase('Addresses', data).then(() => {
 					res.send(data);
 					resolve();
 				})
 			}
 		});
+	});
+});
+
+router.get('/addresses/:address', (req, res) => {
+	const addressStr = req.params.address;
+	const addressesArr = addressStr.split(',');
+	const addressesInfo = [];
+
+	let promises = [];
+
+	addressesArr.forEach(address => {
+		const prom = Addresses.findOne({address}).then(result => {
+			return new Promise(resolve => {
+				if (result) {
+					resolve();
+					addressesInfo.push(result);
+				} else {
+					const data = generateAddressData(address);
+
+					insertIntoDatabase('Addresses', data).then(() => {
+						resolve();
+						addressesInfo.push(data);
+					})
+				}
+			});
+		});
+
+		promises.push(prom);
+	});
+
+	Promise.all(promises.map(p => p.catch(x => console.error(x)))).then(() => {
+		res.send(addressesInfo);
 	});
 });
 
